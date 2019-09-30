706134ae50a5170baf3b94914c5f4af0c3639799
Breno Sapucaia <brenoss.magalhaes@gmail.com>
2019-09-29 22:52:50 -0300

    feature/Usuario

--- sherlock/src/main/java/br/com/techsow/sherlock/model/services/ConnectionFactory.java
+++ sherlock/src/main/java/br/com/techsow/sherlock/model/services/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 
 public class ConnectionFactory {
-	public static  Connection getConnection()throws ClassNotFoundException, SQLException{
+	public static  Connection getConnection() throws ClassNotFoundException, SQLException{
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL","rm83214","280501");
