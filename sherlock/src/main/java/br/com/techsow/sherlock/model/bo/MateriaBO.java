22520b2044e2895540e70e5cc70af6e5b8eef938
ItaloChagas <italocavalcantechagas@gmail.com>
2019-10-13 04:43:17 -0300

    Alterado interface Materia para add materia retornar String

--- sherlock/src/main/java/br/com/techsow/sherlock/model/bo/MateriaBO.java
+++ sherlock/src/main/java/br/com/techsow/sherlock/model/bo/MateriaBO.java
@@ -10,10 +10,63 @@ import br.com.techsow.sherlock.model.interfaces.bo.IMateriaBO;
 public class MateriaBO implements IMateriaBO{
 
 	
-	public int add(Materia obj) {
+	public String add(Materia obj) {
 		
-		return 0;
-	}
+		if(materia.getEmenta().length() > 100) {
+			return "Tamanho da descricao excedeu o limite de caracteres";
+		}
+
+		if(materia.getNome().length() > 100) {
+			return "Nome da matéria maior do que o esperado";
+		}
+
+		if(materia.getId_curso().getId() == 0) {
+			return "ID do curso nao encontrado. Curso não existe";
+		}
+
+		
+		CursoDAO cursoDao = null;
+		try {
+			
+			cursoDao = new CursoDAO();
+			if(cursoDao.getCursoId(materia.getId_curso().getId()) == 1) {
+				return "O curso informado ja contem essa máteria";
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+
+		/////////////////////////////////////////
+		//Espaco para os requisitos funcioinais//
+		/////////////////////////////////////////
+
+
+		MateriaDAO materiaDao = null;
+		int retorno = 0;
+
+		try {
+			materiaDao = new MateriaDAO();
+			retorno = materiaDao.addMateria(materia)	;
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				materiaDao.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+
+		if(retorno == 1) {
+			return "Materia criada com sucesso";
+		}else {
+			return "Materia não pode ser cadastrado. Verificar dados informados";
+		}
+
+	}			}
 
 	
 	public Materia getById(int id) {
