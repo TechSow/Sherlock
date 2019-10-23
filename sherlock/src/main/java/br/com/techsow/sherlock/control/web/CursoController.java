9c20f06da4e81999d46508728f2c736db240bf4f
Breno Sapucaia <brenoss.magalhaes@gmail.com>
2019-10-23 10:40:31 -0300

    Revert "Metodo"
    This reverts commit be479dae21d730977ae8cb61e01f865880200c3d.

--- sherlock/src/main/java/br/com/techsow/sherlock/control/web/CursoController.java
+++ sherlock/src/main/java/br/com/techsow/sherlock/control/web/CursoController.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+<<<<<<< HEAD
 import br.com.techsow.sherlock.model.bo.CursoBO;
 import br.com.techsow.sherlock.model.entities.Curso;
+=======
+import br.com.techsow.sherlock.model.bo.MateriaBO;
+import br.com.techsow.sherlock.model.entities.Materia;
+>>>>>>> parent of be479da... Metodo
 import br.com.techsow.sherlock.model.interfaces.web.Task;
 
 
 public class CursoController  implements Task{
 
 
+<<<<<<< HEAD
 
 	@Override
 	public String processTask(HttpServletRequest req, HttpServletResponse resp) {
@@ -21,3 +27,6 @@ public class CursoController  implements Task{
 	return "materia.jsp";
 	}
 }
+=======
+}
+>>>>>>> parent of be479da... Metodo
