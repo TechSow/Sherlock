62e09526d7480f69076cf751d0aaab3d26d61247
Breno Sapucaia <brenoss.magalhaes@gmail.com>
2019-10-23 23:53:35 -0300

    coloque o lib/ojdbc6.jar na raiz do projeto

--- sherlock/src/main/java/br/com/techsow/sherlock/model/services/ConnectionFactory.java
+++ sherlock/src/main/java/br/com/techsow/sherlock/model/services/ConnectionFactory.java
@@ -9,7 +9,7 @@ import oracle.jdbc.driver.OracleDriver;
 
 public class ConnectionFactory {
 	public static  Connection getConnection() throws ClassNotFoundException, SQLException{
-		
+		// professor você tem que colocar na raiz do projeto lib/ojdbc6.jar
 		DriverManager.registerDriver(new OracleDriver());
 		return DriverManager.getConnection(
 				"jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", 
